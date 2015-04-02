622ca5b891243cd0ef476f6b47737787ed183dfa
Juan Ignacio Vásquez <ji.vasquez0@gmail.com>
2015-04-01 22:27:05 -0300

    WIP integration of Elasticsearch

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,8 @@
+# -*- encoding: utf-8 -*-
+
 from django.db import models
+from elasticsearch_dsl import DocType, String
+
 
 # Create your models here.
 class Region(models.Model):
@@ -11,6 +15,12 @@ class City(models.Model):
 class Brand(models.Model):
     name = models.CharField(max_length=200)
     
+class Publication_index(DocType):
+    title = String(analyzer='snowball')
+    brand = String(analyzer='snowball')
+    class Meta:
+        index = 'publications'
+
 class Publication(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -29,3 +39,37 @@ class Publication(models.Model):
     airbag = models.NullBooleanField()
     abs_break = models.NullBooleanField()
     kilometers = models.IntegerField(null=True)
+    confirmed = models.BooleanField(default=False)
+
+
+    
+
+    CHARACTERISTICS = [
+      { 'key': 'brand', 'label': 'Marca', 'type': 'brand'},
+      { 'key': 'type_of_vehicle', 'label': 'Tipo de vehículo', 'type': 'string'},
+      { 'key': 'vehicle_body', 'label': 'Chasis', 'type': 'string'},
+      { 'key': 'fuel', 'label': 'Tipo de combustible', 'type': 'string'},
+      { 'key': 'at_transmission', 'label': 'Transmisión', 'type': 'custom_boolean', 'alternatives': {'True': 'Automática', 'False': 'Mecánica'}},
+      { 'key': 'air_conditioner', 'label': 'Aire acondicionado', 'type': 'boolean'},
+      { 'key': 'first_owner', 'label': 'Primer dueño', 'type': 'boolean'},
+      { 'key': 'centralized_locking', 'label': 'Cierre centralizado', 'type': 'boolean'},
+      { 'key': 'airbag', 'label': 'Airbag', 'type': 'boolean'},
+      { 'key': 'abs_break', 'label': 'Frenos ABS', 'type': 'boolean'},
+      { 'key': 'kilometers', 'label': 'Kilómetros', 'type': 'integer'}
+    ]
+
+    
+    def get_characteristics(self):
+
+      for characteristic in self.CHARACTERISTICS:
+        if characteristic.get('type') == 'boolean':
+          characteristic['value'] = "Si" if self.__dict__[characteristic.get('key')] else "No"
+        if characteristic.get('type') == 'string':
+          characteristic['value'] = self.__dict__[characteristic.get('key')]
+        if characteristic.get('type') == 'custom_boolean':
+          characteristic['value'] = characteristic.get('alternatives').get('True') if self.__dict__[characteristic.get('key')] else characteristic.get('alternatives').get('False')
+        if characteristic.get('type') == 'integer':
+          characteristic['value'] = self.__dict__[characteristic.get('key')]
+        if characteristic.get('type') == 'brand':
+          characteristic['value'] = self.brand.name
+      return self.CHARACTERISTICS
--- main_app/views.py
+++ main_app/views.py
@@ -2,15 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from main_app.models import Publication
 
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Q
+
 
 def index(request):
     return render(request, "index.html")
 
 
 def results(request):
+    client = Elasticsearch()
+
+    s = Search(using=client, index="publications").query("match", brand="toyota")
+    response = s.execute()
+    for hit in response:
+        print(hit, hit.title)
     results = Publication.objects.all()
     return render(request, "results.html", {'publications':results})
 
 def publication(request, publication_id):
     publication = Publication.objects.get(id=publication_id)
-    return render(request, "publication.html", {'publication':publication})
+    
+    return render(request, "publication.html", {'publication':publication, 'characteristics': publication.get_characteristics()})
